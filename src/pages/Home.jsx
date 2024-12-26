e371357f41d9307bec824914f1796c781f5a030f
Muthu kumaran <129528407+Muthukumaran45@users.noreply.github.com>
2024-12-26 20:24:14 +0530

    added outlet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 function Home() {
     return (
         <div>
             Home
+
+            <section>
+                <Outlet />
+            </section>
         </div>
     )
 }
