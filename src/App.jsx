ef72135daf594501b2c701eb716d5eff08c2be12
Muthukumaran45 <muthukumarann4545@gmail.com>
2025-01-05 20:17:00 +0530

    updated regiesterpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <main>
-      <Outlet />
-    </main>
+    <>
+      <Toaster />
+      <main>
+        <Outlet />
+      </main>
+    </>
   )
 }
 
--- src/pages/RegiesterPage.jsx
+++ src/pages/RegiesterPage.jsx
@@ -9,6 +9,7 @@ import uploadFile from '../helpers/uploadFile';
 // package
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import toast from 'react-hot-toast';
 
 
 function RegiesterPage() {
@@ -42,128 +43,131 @@ function RegiesterPage() {
         setData((prev) => {
             return {
                 ...prev,
-                profile_pic : uploadPhoto?.url
+                profile_pic: uploadPhoto?.url
             }
         })
-}
-
-const handleClearUploadPhoto = (e) => {
-    e.stopPropagation()
-    e.preventDefault()
-    setUploadPhoto(null)
-}
-
-// submit form
-const handleSubmit = async(e) => {
-    e.preventDefault()
-    e.stopPropagation()
-
-    const URL = `${process.env.VITE_BACKEND_URL}/api/register`
-
-    try {
-        const response = await axios.post(URL,data)
-        console.log("register data", data)
-    } catch (error) {
-        console.log('error', error);
-        
     }
 
-    console.log("data", data);
-    
-}
+    const handleClearUploadPhoto = (e) => {
+        e.stopPropagation()
+        e.preventDefault()
+        setUploadPhoto(null)
+    }
+
+    // submit form
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        e.stopPropagation()
+
+        const URL = `${import.meta.env.VITE_BACKEND_URL}/api/register`;
+
+        try {
+            const response = await axios.post(URL, data)
+            console.log("register data", data)
+
+            toast.success(response.data.message)
+        } catch (error) {
+            toast.error(error?.response?.data?.message)
+            console.log('error', error);
+
+        }
+
+        console.log("data", data);
+
+    }
 
 
-return (
-    <div className='mt-5'>
-        <div className='bg-white w-full max-w-md mx:2 rounded overflow-hidden p-4 sm:mx-auto'>
-            <h3>Welcom to chat app!</h3>
+    return (
+        <div className='mt-5'>
+            <div className='bg-white w-full max-w-md mx:2 rounded overflow-hidden p-4 sm:mx-auto'>
+                <h3>Welcom to chat app!</h3>
 
-            <form className='grid gap-4 mt-5' onSubmit={handleSubmit}>
-                {/* name */}
-                <div className='flex flex-col gap-1'>
-                    <label htmlFor='name'>Name :</label>
-                    <input
-                        type='text'
-                        id='name'
-                        name='name'
-                        placeholder='enter your name'
-                        className='bg-slate-100 px-2 py-1 focus:outline-primary'
-                        value={data.name}
-                        onChange={handleOnChange}
-                        required
-                    />
-                </div>
+                <form className='grid gap-4 mt-5' onSubmit={handleSubmit}>
+                    {/* name */}
+                    <div className='flex flex-col gap-1'>
+                        <label htmlFor='name'>Name :</label>
+                        <input
+                            type='text'
+                            id='name'
+                            name='name'
+                            placeholder='enter your name'
+                            className='bg-slate-100 px-2 py-1 focus:outline-primary'
+                            value={data.name}
+                            onChange={handleOnChange}
+                            required
+                        />
+                    </div>
 
-                {/* email */}
-                <div className='flex flex-col gap-1'>
-                    <label htmlFor='email'>Email :</label>
-                    <input
-                        type='email'
-                        id='email'
-                        name='email'
-                        placeholder='enter your email'
-                        className='bg-slate-100 px-2 py-1 focus:outline-primary'
-                        value={data.email}
-                        onChange={handleOnChange}
-                        required
-                    />
-                </div>
+                    {/* email */}
+                    <div className='flex flex-col gap-1'>
+                        <label htmlFor='email'>Email :</label>
+                        <input
+                            type='email'
+                            id='email'
+                            name='email'
+                            placeholder='enter your email'
+                            className='bg-slate-100 px-2 py-1 focus:outline-primary'
+                            value={data.email}
+                            onChange={handleOnChange}
+                            required
+                        />
+                    </div>
 
-                {/* password */}
-                <div className='flex flex-col gap-1'>
-                    <label htmlFor='password'>Password :</label>
-                    <input
-                        type='password'
-                        id='password'
-                        name='password'
-                        placeholder='enter your password'
-                        className='bg-slate-100 px-2 py-1 focus:outline-primary'
-                        value={data.password}
-                        onChange={handleOnChange}
-                        required
-                    />
-                </div>
+                    {/* password */}
+                    <div className='flex flex-col gap-1'>
+                        <label htmlFor='password'>Password :</label>
+                        <input
+                            type='password'
+                            id='password'
+                            name='password'
+                            placeholder='enter your password'
+                            className='bg-slate-100 px-2 py-1 focus:outline-primary'
+                            value={data.password}
+                            onChange={handleOnChange}
+                            required
+                        />
+                    </div>
 
-                {/* profile_pic */}
-                <div className='flex flex-col gap-1'>
-                    <label htmlFor='profile_pic'>Photo :
+                    {/* profile_pic */}
+                    <div className='flex flex-col gap-1'>
+                        <label htmlFor='profile_pic'>Photo :
 
-                        <div className='h-14 bg-slate-200 flex justify-center items-center border hover:border-primary rounded cursor-pointer'>
-                            <p className='text-sm max-w-[300px] text-ellipsis line-clamp-1 flex '>
-                                {
-                                    uploadPhoto?.name ? (uploadPhoto?.name.length > 30 ? `${uploadPhoto?.name.substring(0, 30)}...` : uploadPhoto?.name) : "Upload profile photo"
-                                }
-                                {
-                                    uploadPhoto?.name &&
-                                    <button onClick={handleClearUploadPhoto} className='text-lg ml-2 hover:text-red-600'>
-                                        <X />
-                                    </button>
-                                }
-                            </p>
-                        </div>
+                            <div className='h-14 bg-slate-200 flex justify-center items-center border hover:border-primary rounded cursor-pointer'>
+                                <p className='text-sm max-w-[300px] text-ellipsis line-clamp-1 flex '>
+                                    {
+                                        uploadPhoto?.name ? (uploadPhoto?.name.length > 30 ? `${uploadPhoto?.name.substring(0, 30)}...` : uploadPhoto?.name) : "Upload profile photo"
+                                    }
+                                    {
+                                        uploadPhoto?.name &&
+                                        <button onClick={handleClearUploadPhoto} className='text-lg ml-2 hover:text-red-600'>
+                                            <X />
+                                        </button>
+                                    }
+                                </p>
+                            </div>
 
-                    </label>
+                        </label>
 
-                    <input
-                        type='file'
-                        id='profile_pic'
-                        name='profile_pic'
-                        className='bg-slate-100 px-2 py-1 focus:outline-primary hidden'
-                        onChange={handleUploadPhoto}
-                    />
-                </div>
+                        <input
+                            type='file'
+                            id='profile_pic'
+                            name='profile_pic'
+                            className='bg-slate-100 px-2 py-1 focus:outline-primary hidden'
+                            onChange={handleUploadPhoto}
+                        />
+                    </div>
 
-                <button
-                    className=' bg-primary text-lg px-4 py-1 hover:bg-secondary rounded mt-2 font-bold text-white leading-relaxed tracking-wide'
-                >
-                    Register
-                </button>
-            </form>
+                    <button
+                        className=' bg-primary text-lg px-4 py-1 hover:bg-secondary rounded mt-2 font-bold text-white leading-relaxed tracking-wide'
+                    >
+                        Register
+                    </button>
+                </form>
 
-            <p className='my-3 text-center'>Already have account ? <Link to={"/email"} className='hover:text-primary font-semibold'>Login</Link></p>
+                <p className='my-3 text-center'>Already have account ? <Link to={"/email"} className='hover:text-primary font-semibold'>Login</Link></p>
+            </div>
         </div>
-    </div>
-)
+    )
 }
 
 export default RegiesterPage
